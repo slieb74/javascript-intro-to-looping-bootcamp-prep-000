e69143a968dc5138385a95eaef1ab23a1b0ef4dd
Sam <sammyliebman@gmail.com>
2018-04-17 01:46:51 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -15,10 +15,10 @@ function whileLoop(n){
 function doWhileLoop(array){
   function maybeTrue() {
   return Math.random() >= 0.5
-}
+  }
 do {
     array.pop()
   }
-  while (array.length > 0 && maybeTrue()) {
+  while (array.length > 0 && maybeTrue()); {
   }
 }
